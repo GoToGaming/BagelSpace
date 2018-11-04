381e025fbf2b038400b0cfa3d26fe4e98adf1cb9
MarioGirod <mario.giord@gmail.com>
2018-11-04 15:17:20 +0100

    rocket spund

--- src/Sound.py
+++ src/Sound.py
@@ -10,13 +10,15 @@ class Sound:
 
         mixer.init()
         self.machine_gun_shot = mixer.Sound(os.path.join(sound_folder_path, 'machinegun-single-shot-9db.wav'))
+        self.missile_shot = mixer.Sound(os.path.join(sound_folder_path, 'missile-shot.wav'))
 
     def start_menu_background_music(self):
         if not self.menu_music_is_playing:
+            print('menu sound set')
             file = 'Incoming_Transition.mp3'
             mixer.music.stop()
             mixer.music.load(os.path.join(sound_folder_path, "background", file))
-            mixer.music.play()
+            mixer.music.play(loops=100)
             self.menu_music_is_playing = True
 
     def start_game_background_music(self):
@@ -25,7 +27,7 @@ class Sound:
         file = 'Ether_Oar.mp3'
         mixer.music.stop()
         mixer.music.load(os.path.join(sound_folder_path, "background", file))
-        mixer.music.play()
+        mixer.music.play(loops=100)
 
     def play_machine_gun_shot(self):
         self.machine_gun_shot.play()
